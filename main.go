5b05e4d74b57e2712dc8b0e8cf91b9b66387e5e5
ShuxianMa <934703778@qq.com>
2023-05-04 11:45:38 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 创建处理器函数
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world!", r.URL.Path)
+}
+
+func main() {
+
+}
